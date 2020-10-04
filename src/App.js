2e8ce5367f4699b8aad6b37712fa70ab6a7f4a5f
unknown <yana_586@mail.ru>
2020-10-04 14:14:02 +0300

    Main jsx layouts. Selection in SortPopup, PizzaBlock and Categories.

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react'
+import {Route} from 'react-router-dom'
+import axios from 'axios'
+import {Header} from './components'
+import {Home, Cart} from './pages'
+
+function App() {
+
+	const [pizzas, setPizzas] = useState([])
+
+	useEffect(() => {
+		// fetch('http://localhost:3000/db.json').then(resp => resp.json()).then(json => setPizzas(json.pizzas))
+		axios.get('http://localhost:3000/db.json').then(({data}) => setPizzas(data.pizzas))
+	}, [])
+
+	return (
+		<>
+			<div className="wrapper">
+				<Header/>
+				<div className="content">
+					<Route exact path='/' render={() => <Home items={pizzas} />}/>
+					<Route exact path='/cart' component={Cart}/>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const Categories = ({items}) => {
+
+	const [activeItem, setActiveItem] = useState(null)
+
+	const onSelectItem = (index) => {
+		setActiveItem(index)
+	}
+
+	return (
+		<div className="categories">
+			<ul>
+				<li onClick={() => onSelectItem(null)} className={activeItem === null ? 'active' : ''}>Все</li>
+				{items && items.map((item, index) => <li className={activeItem === index ? 'active' : ''}
+																				onClick={() => onSelectItem(index)} key={item}>{item}</li>)}
+			</ul>
+		</div>
+	)
+}
+
+export default Categories
